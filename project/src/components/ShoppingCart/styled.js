389e172dd360ce77e50cfcb6ec8d01ea63bd2a93
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-12-31 14:41:43 -0300

    implementações de animações, finalizar pedido, tendo o usuário que por o nome e o cpf.

--- project/src/components/ShoppingCart/styled.js
+++ project/src/components/ShoppingCart/styled.js
@@ -11,3 +11,22 @@ export const InputContainer = styled.label`
   align-items: flex-start;
   margin-bottom: 8px;
 `
+export const ShoppingCartContainer = styled.div`
+  border: 1px solid black;
+  padding: 8px;
+`;
+
+export const CartListContainer = styled.div`
+  display: grid;
+  gap: 8px;
+`;
+export const ItemContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 4px;
+  align-items: center;
+
+  p {
+    margin: 0;
+  }
+`;
